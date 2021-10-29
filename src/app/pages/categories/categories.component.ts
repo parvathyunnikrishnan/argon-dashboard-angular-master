a8b1a0163c3002245a43a0217f69d681cfb580a8
Lekshmi J <lekshmi.j@seqato.com>
2021-10-29 19:45:14 +0530

    modified files

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -12,4 +12,51 @@ export class CategoriesComponent implements OnInit {
   ngOnInit() {
   }
 
+}*/
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent implements OnInit {
+
+  constructor(private http: HttpClient ) { }
+  public categories;
+
+  ngOnInit() {
+    const datas = { categoryName: 'Helloooo'}
+  /*  this.http.post('http://157.245.100.86:8080/categories/add', datas).subscribe(
+      (status: any) => {
+
+        console.log("This should be the response???: ", status);
+        
+      },
+      err => {
+
+        if (err.status == 200) {
+         
+
+        }
+      });*/
+    this.http
+    .get('http://157.245.100.86:8080/categories/get-all')
+    .subscribe(
+      (data: any) => {
+        this.categories = data
+        console.log(data)
+      },
+      error => {
+        
+      },
+      () => {
+        // 'onCompleted' callback.
+        // No errors, route to new page here
+      }
+    );
+  }
+
 }
+
